5ca2bb38807fd9d9361f25c53942a776f410ffa1
Ricardo Saldanha <165833459+RicardoSaldanha5@users.noreply.github.com>
2024-05-25 22:14:26 +0100

    desafio casa da gloria

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+
+const vel = 70
+console.log(`a velocidade do carro é de ${vel} km/h`)
+if (vel > 60){//condição simples
+console.log(`ultrapassas-te a velocidade permitida, MULTADO`)
+}
+console.log (`dirige sempre em segurança`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+const país = `Irlanda`
+console.log(`O lugar onde eu vivo é ${país}`)
+if (país == `Portugal`) {
+    console.log(`És Português`)
+}else{
+    console.log(`És Estrangueiro`)
+}
